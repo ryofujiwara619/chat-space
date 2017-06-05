884ca4f21b0812c39bbbba2c23e54cf817994194
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-05 14:51:12 +0900

    Add flash_message when edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ before_action :authenticate_user!
 
   def update
     if @user.update(user_params)
-       redirect_to root_path
+       redirect_to root_path,notice:"アカウント情報を変更しました"
     else
        render 'edit'
     end
@@ -21,4 +21,10 @@ before_action :authenticate_user!
   def set_user
     @user = User.find(params[:id])
   end
+
+  def authenticate_user!
+  unless current_user
+    alert:"ログインしてください"
+  end
+end
 end
