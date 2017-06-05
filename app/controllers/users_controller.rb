ae5bd8d24c93f7e46458e21289e6c393059882ca
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-05 15:26:00 +0900

    Fix flash_message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ before_action :authenticate_user!
 
   def update
     if @user.update(user_params)
-       redirect_to root_path,notice:"アカウント情報を変更しました"
+       redirect_to root_path
+       flash[:notice]="アカウント情報を変更しました"
     else
+       flash[:alert]="アカウント更新に失敗しました。"
        render 'edit'
     end
   end
@@ -22,9 +24,4 @@ before_action :authenticate_user!
     @user = User.find(params[:id])
   end
 
-  def authenticate_user!
-  unless current_user
-    alert:"ログインしてください"
-  end
-end
 end
