e39bab45f1754166b486ca9658cc90ffa3d90f4d
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-06 16:43:14 +0900

    Fix routing again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users,only:[:edit,:update]
-  resources :groups,except:[:destroy,:show] do
+  resources :groups,except:[:destroy] do
     resources :messages,only:[:index.:create]
   end
 end
