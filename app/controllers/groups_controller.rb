cdb65846ce155c164a449c28c73020fc14506265
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-07 13:12:45 +0900

    Add group_create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,10 +9,16 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @group=Group.new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path,notice:"グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +26,10 @@ class GroupsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
