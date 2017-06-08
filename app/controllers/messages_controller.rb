4d82ae8159f9075f9fe3cd59a20cd23764059a31
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-08 17:28:57 +0900

    Fix error_message to use render

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,28 @@
 class MessagesController < ApplicationController
-  def index
-    @groups = Group.all
-    @group = Group.find(params[:group_id]) if params[:group_id]
-    @message = Message.new
+before_action :group_params,only:[:index,:create]
 
+  def index
+    @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      flash[:notice] = "メッセージを送信しました"
-      redirect_to group_messages_path
+      redirect_to group_messages_path,notice: "メッセージを送信しました"
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
-      redirect_to group_messages_path
+      render "messages/index"
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:body, :image).merge(user_id:current_user.id,group_id:params[:group_id])
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id,group_id: params[:group_id])
+  end
+
+  def group_params
+    @groups = Group.all
+    @group = Group.find(params[:group_id])
   end
 
 end
