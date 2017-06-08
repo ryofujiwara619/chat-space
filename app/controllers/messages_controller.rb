b283d92a63ed44782df4845c64ea62aeca21a244
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-08 14:53:00 +0900

    Create message submit function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,24 @@ class MessagesController < ApplicationController
   def index
     @groups = Group.all
     @group = Group.find(params[:group_id]) if params[:group_id]
+    @message = Message.new
+
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "メッセージを送信しました"
+      redirect_to group_messages_path
+    else
+      flash[:alert] = "メッセージ送信に失敗しました。"
+      redirect_to group_messages_path
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id:current_user.id,group_id:params[:group_id])
   end
 
 end
