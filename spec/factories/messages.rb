37b532746426217e4e99a8bf7ed4ce345df23f3c
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-12 17:15:33 +0900

    Create factory for body&image

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
-  
+
   factory :message do
-    body     "aaa"
-    image    "aaa.jpeg"
+    body     Faker::Name.name
+    image    File.open('spec/fixtures/files/sample.png')
     user_id  "1"
     group_id "1"
   end
+
 end
