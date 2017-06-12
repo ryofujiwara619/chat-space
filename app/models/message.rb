16340536b764de01dfbe7cc1ee60223ac244108a
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-12 17:14:34 +0900

    Add validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ApplicationRecord
   belongs_to :group
 
   validates :body, presence:true, unless: :image?
+  validates :user_id, presence:true
+  validates :group_id, presence:true
 
   mount_uploader :image, ImageUploader
 end
