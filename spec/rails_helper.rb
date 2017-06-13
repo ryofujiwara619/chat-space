033efb94eca961000c53e0d113b892eccfdb787e
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-13 15:00:55 +0900

    Settings for test with devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,24 +1,19 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
+require File.expand_path('spec/support/controller_macros.rb')
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactoryGirl::Syntax::Methods
-
   config.use_transactional_fixtures = true
-
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
-
-  config.include Devise::TestHelpers, :type => :controller
-  config.include ControllerMacros, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 end
